cf77f0d64ecec073510131e38fec41b27ef1c504
bungees <84727439+bungeees@users.noreply.github.com>
2024-04-09 14:14:26 -0400

    Update Calculadora.cpp

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -1,20 +1,37 @@
-/ CalculatorTutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    char operador;
+    float num1, num2;
+
+    cout << "Ingrese el operador (+, -, *, /): ";
+    cin >> operador;
+
+    cout << "Ingrese dos números: ";
+    cin >> num1 >> num2;
+
+    switch(operador) {
+        case '+':
+            cout << "El resultado es: " << num1 + num2;
+            break;
+        case '-':
+            cout << "El resultado es: " << num1 - num2;
+            break;
+        case '*':
+            cout << "El resultado es: " << num1 * num2;
+            break;
+        case '/':
+            if(num2 != 0.0)
+                cout << "El resultado es: " << num1 / num2;
+            else
+                cout << "División por cero.";
+            break;
+        default:
+            cout << "Operador no válido";
+            break;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
